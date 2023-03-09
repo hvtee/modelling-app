80e4a17be78abfee141ad1e1f1cf76d23b65ceb5
Artem <milkartem2@mail.ru>
2023-03-09 11:07:22 +0300

    build 1.1.090320231107

--- input.py
+++ input.py
@@ -1,8 +1,7 @@
-# f = 1 * pow(10, 9)
-# omega: float = 2 * 3.14 * pow(10, 9)
 import numpy
 
-omega = numpy.linspace(2 * 3.14 * pow(10, 9), 2 * 3.14 * pow(10, 10), 100)
+# omega = numpy.linspace(6 * pow(10, 9), 6 * pow(10, 10), 540)
+omega: float = 6 * pow(10, 9)
 mu_0: float = 4 * 3.14 * pow(10, -7)
 mu_1: complex = 2 - 2j
 mu_2: complex = 10 - 2j
@@ -11,3 +10,8 @@ a: float = 50 * pow(10, -9)
 ro: float = 0.001
 C: float = 1 * pow(10, -12)
 L: float = 1 * pow(10, -12)
+
+Z_from_mu_consistent = ro + 1j * (omega * L - (1 / (omega * C)))
+Z_from_mu_part_parallel = (ro + 1j * omega * L) / (1j * omega * C * (ro + 1j * (omega * L - (1 / (omega * C)))))
+Z_from_mu_consistent_parallel = ro + (1 / (1j * omega * C + (1 / (1j * omega * L))))
+Z_from_mu_parallel = 1 / (1 / ro + 1j * (omega * C - (1 / (omega * L))))
--- main.py
+++ main.py
@@ -1,16 +1,25 @@
 from formulas import *
 import matplotlib.pyplot as plot
-import numpy
 
-# print("omega: ", omega)
-# print("mu_0: ", mu_0)
-# print("mu_1: ", mu_1)
-# print("mu_2: ", mu_2)
-# print("a: ", a)
-# print("Z: ", Z_from_mu)
-# print("Q: ", Q_mu)
-# print("B: ", B_mu)
-# print("mu_from_omega: ", mu_from_omega)
+print("omega: ", omega)
+print("mu_0: ", mu_0)
+print("mu_1: ", mu_1)
+print("mu_2: ", mu_2)
+print("a: ", a)
+print("Z: ", Z)
+print("Q: ", Q_mu)
+print("B: ", B_mu)
+print("mu_from_omega: ", mu_from_omega)
+
+
+# real = mu_from_omega.real
+# imag = mu_from_omega.imag
+#
+# plot.plot(omega / (2 * 3.14), real, 'r')
+# plot.plot(omega / (2 * 3.14), imag, 'g')
+# plot.ylabel('u')
+# plot.xlabel('w/2pi, Hz')
+# plot.show()
 
 # plot.plot(omega, mu_from_omega)
 # plot.show()
@@ -19,19 +28,7 @@ import numpy
 # x = [ele.real for ele in mu_from_omega]
 # # extract imaginary part
 # y = [ele.imag for ele in mu_from_omega]
-#
 # # plot the complex numbers
 # plot.scatter(x, y)
-# plot.ylabel('Imaginary')
-# plot.xlabel('Real')
+
 # plot.show()
-
-# extract real part using numpy array
-x = mu_from_omega.real
-# extract imaginary part using numpy array
-y = mu_from_omega.imag
-
-# plot the complex numbers
-plot.plot(x, 'r')
-plot.plot(y, 'g')
-plot.show()
