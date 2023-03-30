4c4044ad172c454a825612e92f8f48c0f968bd2f
Artem <milkartem2@mail.ru>
2023-03-30 14:03:22 +0300

    merged

--- formulas.py
+++ formulas.py
@@ -19,42 +19,6 @@ def graph(x, y, str):
     plot.grid(True)
 
 
-# def solve_mu(number_cd, number_id):
-#     Q_mu = (1 / CommonData.common_data[number_cd].mu_2) - (
-#             (1j * CommonData.common_data[number_cd].omega * CommonData.common_data[number_cd].a *
-#              CommonData.common_data[number_cd].mu_0) / (2 * ImpedanceData.impedance_data[number_id].Z))
-#
-#     B_mu = ((3 - 5 * CommonData.common_data[number_cd].c) - CommonData.common_data[number_cd].mu_1 * Q_mu * (
-#             6 - 7 * CommonData.common_data[number_cd].c)) / (3 - 2 * CommonData.common_data[number_cd].c)
-#
-#     mu_from_omega = (-B_mu + pow(B_mu * B_mu + 8 * CommonData.common_data[number_cd].mu_1 * Q_mu, 0.5)) / (4 * Q_mu)
-#
-#     graph(CommonData.common_data[number_cd].omega, mu_from_omega, "mu")
-#
-#
-# def solve_epsilon(number_cd, number_id):
-#     epsilon_1 = 1 - 1j * (CommonData.common_data[number_cd].sigma_1 / (
-#             CommonData.common_data[number_cd].omega * CommonData.common_data[number_cd].epsilon_0))
-#
-#     epsilon_2 = 2 - 1j * (CommonData.common_data[number_cd].sigma_2 / (
-#             CommonData.common_data[number_cd].omega * CommonData.common_data[number_cd].epsilon_0))
-#
-#     Q_epsilon = (ImpedanceData.impedance_data[number_id].Z * CommonData.common_data[number_cd].omega *
-#                  CommonData.common_data[number_cd].a * CommonData.common_data[number_cd].epsilon_0) \
-#                 / \
-#                 (1j * 2 + ImpedanceData.impedance_data[number_id].Z * CommonData.common_data[number_cd].omega
-#                  * CommonData.common_data[number_cd].a * CommonData.common_data[number_cd].epsilon_0
-#                  * epsilon_2)
-#
-#     B_epsilon = (3 * CommonData.common_data[number_cd].c - 1 - epsilon_1 * Q_epsilon * (
-#             3 * CommonData.common_data[number_cd].c - 2))
-#
-#     epsilon_from_omega = (B_epsilon + pow(
-#         B_epsilon * B_epsilon + 8 * epsilon_1 * Q_epsilon, 0.5)) / (4 * Q_epsilon)
-#
-#     graph(CommonData.common_data[number_cd].omega, epsilon_from_omega, "epsilon")
-
-
 def solve(number_cd, number_id):
     omega = CommonData.common_data[number_cd].omega
     mu_0 = CommonData.common_data[number_cd].mu_0
